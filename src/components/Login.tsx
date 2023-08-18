7ed0780d9dc533982e03322d656edfa509c66bfe
Elielton Ramos <elieltonramos14@gmail.com>
2023-08-18 15:50:31 -0300

    Criando componente de login

---
+++ src/components/Login.tsx
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <form>
+      <input type="text" data-testid="email-input" />
+      <input type="password" data-testid="password-input" />
+      <button type="submit" data-testid="login-submit-btn">Enter</button>
+    </form>
+  );
+}
+
+export default Login;
