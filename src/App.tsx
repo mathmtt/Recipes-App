cc2c8390c9ea0932a413ef7928a4bebf79604bcd
markoclimako <mkclimako@gmail.com>
2023-08-18 22:37:29 -0300

    criado o layout da aplicação para as rotas que irão renderizar o componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import Layout from './component/Layout';
+import DoneRecipes from './pages/DoneRecipes';
 import Drinks from './pages/Drinks';
-import Login from './pages/Login/Login';
-import Meals from './pages/Meals/Meals';
+import FavoritesRecipes from './pages/FavoritesRecipes';
+import Login from './pages/Login';
+import Meals from './pages/Meals';
 import Profile from './pages/Profile';
+
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <Login /> } />
-        <Route path="/meals" element={ <Meals /> } />
-        <Route path="/drinks" element={ <Drinks /> } />
-        <Route path="/profile" element={ <Profile /> } />
+        <Route element={ <Layout /> }>
+          <Route path="/meals" element={ <Meals /> } />
+          <Route path="/drinks" element={ <Drinks /> } />
+          <Route path="/profile" element={ <Profile /> } />
+          <Route path="/done-recipes" element={ <DoneRecipes /> } />
+          <Route path="/favorite-recipes" element={ <FavoritesRecipes /> } />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
