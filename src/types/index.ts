16e97caf37c4ddc27cc7b0b8121dbb014b0bdc9b
markoclimako <mkclimako@gmail.com>
2023-08-18 22:35:29 -0300

    criada a pasta types e definidas as tipagens de PathNameProps e PathNames

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+type PathNameProps = {
+  title: string,
+  icon: string
+};
+export type PathNames = {
+  meals: PathNameProps;
+  drinks: PathNameProps;
+  profile: PathNameProps;
+  'done-recipes': PathNameProps;
+  'favorite-recipes': PathNameProps;
+};
